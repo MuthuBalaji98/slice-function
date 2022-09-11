8b445f3a60f2909d37adc999bb34fd510c08bdea
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-11 21:17:17 +0530

    slice

---
+++ main.js
@@ -0,0 +1,7 @@
+let subject=["Tamil","English","Maths","Science","Social"];
+console.log("original array :",subject);
+console.log("length of the array is "+subject.length);
+console.log("method : array.slice()");
+result=subject.slice(1,4);
+console.log("change in array : ", result);
+console.log("return: length of array after slice() is "+result.length);
